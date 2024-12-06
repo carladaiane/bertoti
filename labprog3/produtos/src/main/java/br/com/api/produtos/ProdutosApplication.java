2689838723d42dd0ca7ffc6eb18951192dced7a4
Carla Araujo <daianecarla.222@gmail.com>
2024-12-06 03:27:02 -0300

    Cria serviço para cadastrar produtos

--- labprog3/produtos/src/main/java/br/com/api/produtos/ProdutosApplication.java
+++ labprog3/produtos/src/main/java/br/com/api/produtos/ProdutosApplication.java
@@ -9,5 +9,5 @@ public class ProdutosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProdutosApplication.class, args);
 	}
-
+	
 }
--- labprog3/produtos/src/main/java/br/com/api/produtos/controler/ProdutoControler.java
+++ labprog3/produtos/src/main/java/br/com/api/produtos/controler/ProdutoControler.java
@@ -1,11 +1,26 @@
 package br.com.api.produtos.controler;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.model.ProdutoModel;
+import br.com.api.produtos.service.ProdutoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class ProdutoControler {
+
+    @Autowired
+    private ProdutoService ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModel>listar() {
+        return ps.listar();
+    }
+    
+
 
         @GetMapping("/")
         public String rota() {
